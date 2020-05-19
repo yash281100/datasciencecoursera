e76968a94f425e3764be21eb0d2a48eaf47f1579
Yash <yashchaud@gmail.com>
2020-05-19 12:12:12 +0530

    Added Rassignment2.R file

--- Rassignment2.R
+++ Rassignment2.R
@@ -1,4 +1,9 @@
-##Please include your own comment to explain your code (Required in Rubric)
+##This function Function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object
+#can cache its own object. 
+
+#<<- operator is used to assign a value to an object in an environment that is different 
+#from the current environment 
 
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
@@ -14,7 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
   getInverse = getInverse)
 }
 
-##Please include your own comment to explain your code (Required in Rubric)
+## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an 
+# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not.
+# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from 
+# and set the invertible  matrix by using the solve function.
+# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works
+#after running the code 1st time), it returns a message  "Getting Cached Invertible Matrix" 
+#and the cached object
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
